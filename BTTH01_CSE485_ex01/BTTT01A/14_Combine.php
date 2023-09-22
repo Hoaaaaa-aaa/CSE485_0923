874faa3348fdf7c679c1f6f8132f8a18cec4b3e7
Diệu Hoa <126698752+Hoaaaaa-aaa@users.noreply.github.com>
2023-09-22 09:16:20 +0700

    Add files via upload

--- BTTH01_CSE485_ex01/BTTT01A/14_Combine.php
+++ BTTH01_CSE485_ex01/BTTT01A/14_Combine.php
@@ -2,15 +2,11 @@
 $array1 = [[77, 87],[23, 45]];
 $array2 = ['giá trị 1', 'giá trị 2'];
 
-// Tách mảng 1 thành 2 mảng con
-$array11 = $array1[0];
-$array12 = $array1[1];
-
-
 $result = [];
 
-foreach ($array2 as $key => $value) {
-    $result[] = [$value] + $array11[$key];
+foreach ($array1 as $key => $subArray) {
+    $result[] = array_merge([$array2[$key]], $subArray);
 }
+
 echo "<pre>".print_r($result, true)."</pre>";
 ?>
